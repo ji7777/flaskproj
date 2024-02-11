23436432c823277762416fd34cfd2ae23ec2fcdb
ji7777 <jishil7777@gmail.com>
2024-02-11 19:53:48 +0530

    new changes

--- login.py
+++ login.py
@@ -1,26 +1,87 @@
-from flask import Flask,render_template,url_for,request,redirect
-import jsonify
+from flask import Flask,render_template,url_for,request,redirect,session
+import jsonify,pickle,random
 import pandas as pd
-from other import inser_new
+from other import expense,valid_login,valid
+from datetime import timedelta
+import matplotlib.pyplot as plt
 app=Flask(__name__,template_folder='template',static_folder='static')
-@app.route('/')
+app.secret_key='newwork'
+@app.route('/',methods=["GET","POST"])
 def hi():
-    return render_template('work.html',i="login")
+    err=None
+    if request.method=="POST":
+        x=request.form["uid"]
+        session['uid']=x
+        err=valid_login(x)
+        b=expense(x)
+        session['obj']=pickle.dumps(b)
+        if err is not None:
+            return redirect(url_for('ok'))
+        else:
+            err="wrong uid"
+    return render_template('work.html',err=err,i="login")
 @app.route('/endd',methods=["GET","POST"])
 def helo():
-    e=None
+    err=None
+    uid=None
     if request.method=="POST":
         x=request.form.to_dict()
-        if x is None:
-            e="Try again"
-        else:
+        e=valid(x['username'])
+        print(e)
+        if e is None:
             print(x)
-            inser_new(x['user'],x['email'])
-            return redirect(url_for('ok'))
-    return render_template('signup.html',e=e,i="sign up")
-@app.route('/home')
+            err=None
+            uid=x['username']+str(random.randint(0,10000))+x['email'][1:5]
+            session['uid']=uid
+            b=expense(uid)
+            b.inser_new(x['username'],x['email'],uid,x['salary'])
+        else:
+            err=e
+            uid=None
+        print(err)
+    return render_template('signup.html',e=err,i="sign up",uid=uid)
+@app.route('/home',methods=["GET","POST"])
 def ok():
-    return render_template('homepage.html',i="expense calc")
+    u=None
+    f=''
+    i=''
+    data=[]
+    l=session.get("obj")
+    z=pickle.loads(l)
+    if request.method=="POST":
+        x=request.form["expense"]
+        y=request.form["cost"]
+        r=request.form["rec"]
+        print(session)
+        f=session['uid']
+        z.insert_exp(x,y,r)
+        print("DATA INSERTED")
+        return redirect(url_for('ok'))
+    i,data=z.view()
+    z.viz()
+        
+    
+    print(session,i,data)
+    if 'uid' not in session:
+        return redirect(url_for('log'))
+    
+    return render_template('homepage.html',uid=f,i="expense calc",u=u,rem=i,data=data)
+
+    # z=session.get("obj")
+        
+    # u=pickle.loads(z)
+            
+    # print(session)
+    # f=session.get('uid',None)
+    
+@app.route('/logout')
+def log():
+    session.pop('uid', None)
+    session.pop('data_inserted', None)
+    return render_template('logout.html')
+
+def run_flask_app():
+    app.run(debug=True,threaded=True)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    run_flask_app()
--- other.py
+++ other.py
@@ -1,5 +1,12 @@
 import snowflake.connector,os
 from dotenv import load_dotenv
+import matplotlib
+import numpy as np
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import pandas as pd,datetime as dt
+import matplotlib.dates as mdates
+
 load_dotenv()
 con=snowflake.connector.connect(user=os.environ.get('user'),
 password=os.environ.get('password'),
@@ -11,8 +18,102 @@ cur=con.cursor()
 x=cur.execute("select * from exp_login")
 y=x.fetchall()
 print(y)
-def inser_new(a,b):
-    cur.execute('insert into exp_login values(?,?)',(a,b))
-    con.commit()
-cur.close()
-con.close()
+class expense:
+    def __init__(self,uid):
+        self.uid=uid
+    def inser_new(self,uid,username,email,sal):
+        self.username=username
+        self.email=email
+        self.sal=sal
+        cur.execute('insert into exp_login values(%s,%s,%s,%s)',(self.username,self.email,self.uid,self.sal))
+        con.commit()
+    
+    def insert_exp(self,desc,cost,rec=None):
+        self.desc=desc
+        self.cost=cost
+        self.rec=rec
+        if self.rec=='':
+            self.rec=0
+        elif self.cost=='':
+            self.cost=0
+        print(self.desc,self.cost,self.rec)
+        t=dt.date.today().isoformat()
+        cur.execute('insert into expense values(%s,%s,%s,%s,%s)',(self.desc,self.cost,self.uid,self.rec,t))
+        con.commit()
+    def view(self):
+        cur.execute('select * from expense where uid=%s',self.uid)
+        f=cur.fetchall()
+        y=pd.DataFrame(data=f,columns=['desc','cost','uid','rec','date'])
+        
+        cur.execute('select salary,uid from exp_login where uid=%s',self.uid)
+        z=pd.DataFrame(data=cur.fetchall(),columns=['salary','uid'])
+        
+        o=pd.merge(y,z,how='left',on='uid')
+        e=o.groupby(['uid','salary'])[['cost','rec']].sum().reset_index()
+        e.columns = ['uid', 'salary', 'totalcost', 'totalrec']
+        re=[]
+        p=0
+        q=0
+        for i,row in o.iterrows():
+            if row['cost'] is None:
+                p=0
+            if row['rec'] is None:
+                q=0
+            else:
+                q+=row['rec']
+            if row['cost'] is not None:
+                p+=row['cost']
+            re.append(row['salary']-p+q)
+        o['rem']=re
+        if e.empty:
+            e=None
+        else:
+            e=int((e['salary']-e['totalcost']+e['totalrec']).iloc[0])
+        return e,o[['desc','cost','rec','salary','rem','date']]
+    def viz(self):
+        e, d = self.view()
+        fig1 = plt.figure(1,facecolor='lightgrey')
+        w = 0.5
+        grouped_data = d.groupby('date').sum().reset_index()
+        print(grouped_data)
+# Get the unique dates and their corresponding indices
+        dates = np.arange(len(grouped_data['date']))
+
+# Plot the bars using the grouped data
+        plt.bar(dates, grouped_data['cost'], w, label="cost")
+        plt.bar(dates+w, grouped_data['rec'], w, label="rec")
+        # Calculate positions for bars
+        # dates = np.arange(len(d['date'].unique().astype('str').tolist()))
+        # print(dates,len(d['cost']),len(d['rec']),d['date'])
+        # plt.bar(dates, d['cost'], w, label="cost")
+        # plt.bar(dates + w, d['rec'], w, label="rec")
+
+        # Plot expected salary
+        if not d.empty:
+            x = d['salary'].iloc[0]
+            plt.axhline(x, color='r', linestyle='--', label="expected")
+
+        plt.xlabel('Date', color='r')
+        plt.ylabel('Money', color='r')
+        plt.xticks(dates + w / 2, d['date'].unique().astype('str').tolist())  # Set x-ticks
+        plt.legend()
+        plt.savefig('static/new.png')
+        plt.close()
+
+        return 1
+
+def valid_login(uid):
+    cur.execute('select * from exp_login where uid=%s',uid)
+    f=cur.fetchall()
+    print(f)
+    if f:
+        return "valid"
+    else:
+        return None
+def valid(a):
+        cur.execute('select * from exp_login where username=%s',(a))
+        y=cur.fetchall()
+        if y:
+            return "User already exist. Try new email id"
+        else:
+            return None
